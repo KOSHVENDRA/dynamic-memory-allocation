76b31abbfe14f42511fbd6c13c5537e9265041fd
Koshvendra Singh <koshvendra1999@gmail.com>
2019-12-03 04:49:33 +0000

    memory allocation using calloc function

---
+++ calloc.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+double  * kosh(int *data ,int n){
+  double sum=0;int i;
+  for(i=0;i<100;i++)
+    
+    {sum=sum+data[i];
+    }
+  double mean=sum/100;
+
+double var=0;
+for(i=0;i<100;i++)
+
+  {
+    var=var+(data[i]*data[i]);
+
+  }
+double variance=var/100;
+
+ static double val[2];
+ val[0]=mean;
+   val[1]=variance;
+   printf("%f",val[0]);
+   return val;
+
+}
+int main()
+{
+  int i,*p;
+  p=(int*)calloc(100,sizeof(int));  for(i=1;i<=100;i++)
+    {
+      printf("%d\n ",p[i-1]);
+      p[i-1]=i*i;
+      
+      
+    }
+  double *poin;
+  poin=kosh(p,100);
+  printf("\nmean is %f\nvar. is %f",poin[0],poin[1]);
+  free(p);
+  return 0;
+
+}
+
+//every element became zero by replacing malloc function to calloc function
