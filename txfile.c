128d99b7344e5cedd6f65caab87ff57e929b8225
Koshvendra Singh <koshvendra1999@gmail.com>
2019-12-03 04:51:46 +0000

    result in text file

---
+++ txfile.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+double  * kosh(int *data ,int n){
+  double sum=0;int i;
+  for(i=0;i<100;i++)
+    
+    {sum=sum+data[i];
+    }
+  double mean=sum/100;
+
+double var=0;
+for(i=0;i<100;i++)
+
+  {
+    var=var+(data[i]*data[i]);
+
+  }
+double variance=var/100;
+
+ static double val[2];
+ val[0]=mean;
+   val[1]=variance;
+   printf("%f",val[0]);
+   return val;
+
+}
+int main()
+{FILE *po;
+  
+  int i,*p;
+  p=(int*)malloc(100*sizeof(int));
+  for(i=1;i<=100;i++)
+    {
+
+      p[i-1]=i*i;
+      printf("%d\n ",p[i-1]);
+      
+    }
+  double *poin;
+  poin=kosh(p,100);
+  printf("\nmean is %f\nvar. is %f",poin[0],poin[1]);
+  po=fopen("out_dat.txt","w");
+  fprintf(po,"\nmean is %f\nvar is %f",poin[0],poin[1]);
+  
+  free(p);
+  return 0;
+
+}
+
+
+
