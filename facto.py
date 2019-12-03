03cee4232e6f4918637888397aaeb013dfd11259
Koshvendra Singh <koshvendra1999@gmail.com>
2019-12-03 05:00:57 +0000

    factorial in python using c code

---
+++ facto.py
@@ -0,0 +1,10 @@
+import time
+
+from ctypes import*
+fat=CDLL('./functon.so')
+start_time=time.time()
+f=fat.fact(45)
+print(f)
+end_time=time.time()
+k=end_time-start_time
+print("\ntime taken by code",k)
